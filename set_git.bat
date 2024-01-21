4543ac41b09eeaa2ebf7c00559e18c84d061764a
Noymalka <Noy.malka@e.braude.ac.il>
2024-01-21 17:14:04 +0200

    Your commit message here

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/Noymalka/web.git
+git remote add origin https://github.com/Noymalka/webCourse.git
 git push -u origin main
